e6b3361c5cc4476d3d6eed604a61d7cabbfcd37b
Rafael Medina <rmedinap@gmail.com>
2017-04-27 10:23:28 -0500

    Planteando Ejemplo02 de los Ejercicios02.

---
+++ src/Ejercicios02/Ejemplo02.java
@@ -0,0 +1,12 @@
+package Ejercicios02;
+
+public class Ejemplo02 {
+    public static void main(String[] args) {
+        //Ingresar el estado civil de la persona (soltero o casado)
+        //Si la persona es soltera
+        // su salario neto sera igual a su salario base mas 100 soles
+        //Si la persona es casada
+        // su salario Neto sera igual a su salario base mas 300 soles
+    }
+    
+}
